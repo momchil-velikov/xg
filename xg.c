10b588678c2e0144e57f6339c85e509397d3fda6
Momchil Velikov <velco@fadata.bg>
2006-01-19 14:33:29 +0000

    add command line options

--- xg.c
+++ xg.c
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with XG; if not, write to the Free Software Foundation,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 #include "grammar.h"
 #include "lr0.h"
@@ -25,26 +25,164 @@
 #include "xg.h"
 
 #include <ulib/cache.h>
+#include <ulib/options.h>
 #include <stdio.h>
 
 /* XG message log.  */
 ulib_log *xg_log;
 
+/* Instrument the parser for debugging info.  */
+int xg_flag_debug = 0;
+
+/* Prefix for all externally visible symbols.  */
+const char *xg_namespace = 0;
+
+/* Input file name.  */
+const char *xg_input = 0;
+
+/* Output file name.  */
+const char *xg_output = 0;
+
+/* Sentence size.  */
+int xg_sentence_size = 10;
+
+/* Report file name.  */
+const char *xg_report = 0;
+
+/* Generate report.  */
+int xg_flag_report = 0;
+
+/* Output type.  */
+enum output_type { output_defines = 1, output_slr, output_lalr,
+                   output_random_sentence };
+int xg_flag_output_type = output_lalr;
+
+
 static int
-usage ()
+print_version ()
 {
-  fprintf (stderr, "usage: xg <filename>\n");
+  fputs ("xg (XG) 0.1 (alpha)\n", stderr);
+  fputs ("Copyright (C) 2005, 2006 Momchil Velikov\n", stderr);
+  fputs ("This is free software; see the source for copying conditions."
+         "  There is NO\n", stderr);
+  fputs ("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR"
+         " PURPOSE.\n", stderr);
   return -1;
 }
 
-int
-main (int argc, char *argv [])
+static ulib_option options [];
+static int
+print_usage ()
 {
-  static ulib_log log;
-  xg_grammar *g;
+  fputs ("usage: xg [<options>] <grammar>\n", stderr);
+  fputs ("Options are:\n", stderr);
+  ulib_options_help (stderr, options);
 
-  if (argc != 2)
-    return usage ();
+  return -1;
+}
+
+static int
+handle_namespace (const char *arg)
+{
+  xg_namespace = arg;
+  return 0;
+}
+
+static int
+handle_output (const char *arg)
+{
+  xg_output = arg;
+  return 0;
+}
+
+static int
+handle_sentence_size (const char *arg)
+{
+  long sz;
+  char *end;
+
+  sz = strtol (arg, &end, 0);
+  if (*end != 0)
+    {
+      fprintf (stderr, "xg: ERROR: invalid argument to --sentence\n");
+      return -1;
+    }
+  xg_sentence_size = sz;
+  return 0;
+}
+
+static int
+handle_report (const char *arg)
+{
+  xg_report = arg;
+  return 0;
+}
+
+static int
+handle_non_option_arg (const char *arg)
+{
+  if (xg_input)
+    fprintf (stderr, "xg: WARNING: input file ``%s'' overrides ``%s''\n",
+             arg, xg_input);
+  xg_input = arg;
+  return 0;
+}
+
+/* Command line options.  */
+static ulib_option options [] =
+{
+  { .key = 'V', .name = "version", .cb = print_version,
+    .help = "\toutput version information and exit" },
+
+  { .key = 'h', .name = "help", .cb = print_usage,
+    .help = "\t\tdisplay this help and exit" },
+
+  { .key = 't', .name = "debug", .flag = &xg_flag_debug, .value = 1,
+    .help = "\t\tinstrument the parser for debugging (TODO)" },
+
+  { .key = 'n', .name = "namespace", .cb = handle_namespace,
+    .flags = ulib_option_required_arg, .arg = "<name>",
+    .help = "\n\t\t\tset the namespace for the external symbols (TODO)" },
+
+  { .key = 'o', .name = "output", .cb = handle_output,
+    .flags = ulib_option_required_arg, .arg = "<path>",
+    .help = "\n\t\t\tname output file" },
+
+  { .key = 'D', .name = "defines", .flag = &xg_flag_output_type,
+    .value = output_defines,
+    .help = "\toutput a header file with token #defines (TODO)" },
+
+  { .key = 'S', .name = "slr", .flag = &xg_flag_output_type,
+    .value = output_slr,
+    .help = "\t\toutput an SLR(1) parser" },
+
+  { .key = 'L', .name = "lalr", .flag = &xg_flag_output_type,
+    .value = output_lalr,
+    .help = "\t\toutput a LALR(1) parser" },
+
+  { .key = 's', .name = "sentence", .flag = &xg_flag_output_type,
+    .value = output_random_sentence,
+    .help = "\tgenerate random sentence" },
+
+  { .key = 'z', .name = "sentence-size", .cb = handle_sentence_size,
+    .flags = ulib_option_required_arg, .arg = "<number>",
+    .help = "\n\t\t\trandom sentence size" },
+
+  { .key = 'r', .name = "report", .flag = &xg_flag_report, .value = 1,
+    .cb = handle_report, .flags = ulib_option_optional_arg, .arg = "<path>",
+    .help = "\n\t\t\tproduce a report with details on the parser" },
+
+  { .key = 0, .cb = handle_non_option_arg }
+};
+
+int
+main (int argc, const char *argv [])
+{
+  int sts;
+  static ulib_log log;
+  xg_grammar *g = 0;
+  xg_lr0dfa *dfa = 0;
+  FILE *out;
 
   /* Create the message log.  */
   xg_log = &log;
@@ -54,45 +192,103 @@ main (int argc, char *argv [])
       return -1;
     }
 
+  argv [0] = "xg";
+  if (ulib_options_parse (options, argc, argv, stderr) < 0)
+    return -1;
+
+  /* Check options sanity.  */
+  if (! xg_input)
+    {
+      fputs ("xg: ERROR: missing input file name\n", stderr);
+      return -1;
+    }
+
   /* Initialize memory management.  */
   if (xg__init_grammar () < 0 || xg__init_lr0dfa () < 0 || xg__init_lalr () < 0)
     goto error;
 
   /* Parse the input file. */
-  if ((g = xg_grammar_read (argv [1])) == 0)
+  if ((g = xg_grammar_read (xg_input)) == 0)
     goto error;
 
   /* Compute FIRST and FOLLOW sets.  */
   if (xg_grammar_compute_first (g) < 0 || xg_grammar_compute_follow (g) < 0)
     goto error;
 
-  if (1)
+  /* Create the parsing automaton.  */
+  if (xg_flag_output_type == output_slr || xg_flag_output_type == output_lalr)
     {
-      xg_lr0dfa *dfa = xg_lr0dfa_new (g);
-      xg_make_lalr_reductions (g, dfa);
+      if ((dfa = xg_lr0dfa_new (g)) == 0)
+        goto error;
+
+      if ((xg_flag_output_type == output_slr
+           && xg_make_slr_reductions (g, dfa) < 0)
+          || (xg_flag_output_type == output_lalr
+              && xg_make_lalr_reductions (g, dfa) < 0))
+        goto error;
+
       xg_resolve_conflicts (g, dfa);
-      xg_grammar_debug (stderr, g);
-      xg_lr0dfa_debug (stderr, g, dfa);
-      xg_gen_c_parser (stdout, g, dfa);
-      xg_lr0dfa_del (dfa);
     }
-  else if (0)
-    {
-      xg_make_random_sentence (stdout, g, 10);
-    }
+
+  /* Open the output file.  */
+  if (xg_output == 0)
+    out = stdout;
   else
     {
-      xg_grammar_debug (stderr, g);
+      if ((out = fopen (xg_output, "w")) == 0)
+        {
+          fprintf (stderr, "xg: ERROR: Cannot open output file  ``%s''\n",
+                   xg_output);
+          goto error;
+        }
     }
-  xg_grammar_del (g);
+
+  /* Write the required output.  */
+  if (xg_flag_output_type == output_random_sentence)
+    xg_make_random_sentence (out, g, xg_sentence_size);
+  else if (xg_flag_output_type == output_defines)
+    {
+    }
+  else
+    xg_gen_c_parser (out, g, dfa);
+
+  if (xg_output)
+    fclose (out);
+
+  /* Report details about the parser.  */
+  if (xg_flag_report)
+    {
+      if (xg_report)
+        {
+          if ((out = fopen (xg_report, "w")) == 0)
+            goto error;
+        }
+      else
+        out = stderr;
+
+      xg_grammar_debug (out, g);
+      if (dfa)
+        xg_lr0dfa_debug (out, g, dfa);
+
+      if (xg_report)
+        fclose (out);
+    }
+
+  sts = 0;
+  goto exit;
+
+error:
+  sts = -1;
+exit:
+  if (dfa)
+    xg_lr0dfa_del (dfa);
+
+  if (g)
+    xg_grammar_del (g);
   ulib_gcrun ();
 
   ulib_log_write (xg_log, stderr);
-  return 0;
-
-error:
-  ulib_log_write (xg_log, stderr);
-  return -1;
+  return sts;
 }
 
 /*
